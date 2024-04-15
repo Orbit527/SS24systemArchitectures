1e5731c9a292b8c0932992a1429d8eb08181f2b0
Orbit527 <peterschneider15@gmx.net>
2024-04-15 10:24:34 +0200

    added Exception handling on commandClient

---
+++ src/main/java/at/fhv/lab1/commandclient/exceptions/CustomerNotCreatableException.java
@@ -0,0 +1,7 @@
+package at.fhv.lab1.commandclient.exceptions;
+
+public class CustomerNotCreatableException extends Exception{
+    public CustomerNotCreatableException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/at/fhv/lab1/commandclient/exceptions/NotBookableException.java
@@ -0,0 +1,7 @@
+package at.fhv.lab1.commandclient.exceptions;
+
+public class NotBookableException extends Exception{
+    public NotBookableException(String message) {
+        super(message);
+    }
+}
